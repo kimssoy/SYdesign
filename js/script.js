f4769722cffa73a3d99a270ee4377b4e5a14f400
kimssoy <97716636+kimssoy@users.noreply.github.com>
2022-02-07 18:38:36 +0900

    test

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
+$(function(){
+  $(".nav>li>a").click(function(){
+    $('.nav').css("display","none");
+  })
 
+  $(".toggle").click(function(){
+      $(".nav").slideToggle()
+      $(".change").css("display","block");
+      $(".one").css("display","none");
+  });
+
+  $(".change").click(function(){
+      $(".one").css("display","block");
+      $(".change").css("display","none");
+  });
+
+});
