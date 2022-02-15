9ba6da54c1bfd3858a1e7468ce08b5e913ede1b3
kimssoy <97716636+kimssoy@users.noreply.github.com>
2022-02-15 16:55:29 +0900

    test

--- js/script.js
+++ js/script.js
@@ -1,25 +1,27 @@
 $(function(){
-  $(".toggle").click(function(){
-    $(".nav").slideToggle()
-  });
+    $(".toggle").click(function(){
+        $(".nav").slideToggle()
+    });
 
-  $(window).resize(function(){
-    let winW = window.innerWidth;
-    if(winW > 768){
-      $(".nav>li>a").click(function(){
-        $(".nav").css("display","block");
-      });
-      $(".logo>a").click(function(){
-        $(".nav").css("display","block");
-      });
-    }else{
-      $(".nav>li>a").click(function(){
-        $(".nav").css("display","none");
-      });
-      $(".logo>a").click(function(){
-        $(".nav").css("display","none");
-      });
-    }
-});
-$(window).trigger("resize");
+    $(window).resize(function(){
+        let winW = window.innerWidth;
+        if(winW > 768){
+            $(".nav").css("display","flex");
+            $(".nav>li>a").click(function(){
+                $(".nav").css("display","block");
+            });
+            $(".logo>a").click(function(){
+                $(".nav").css("display","block");
+            });
+        }else{
+            $(".nav").css("display","none");
+            $(".nav>li>a").click(function(){
+                $(".nav").css("display","none");
+            });
+            $(".logo>a").click(function(){
+                $(".nav").css("display","none");
+            });
+        }
+    });
+    $(window).trigger("resize");
 });
